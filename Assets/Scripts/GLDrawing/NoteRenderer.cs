391fa311157215924134e4ac8633dfbbd07e67e6
katout <aokaiinto@gmail.com>
2021-08-21 02:49:54 +0900

    手本を複製して描画

--- Assets/Scripts/GLDrawing/NoteRenderer.cs
+++ Assets/Scripts/GLDrawing/NoteRenderer.cs
@@ -7,22 +7,24 @@ namespace NoteEditor.GLDrawing
 {
     public class NoteRenderer : MonoBehaviour
     {
+        [SerializeField] Color modelColor;
+
         void LateUpdate()
         {
             if (Audio.Source.clip == null)
                 return;
 
+            var min = ConvertUtils.ScreenToCanvasPosition(Vector3.zero).x;
+            var max = ConvertUtils.ScreenToCanvasPosition(Vector3.right * Screen.width).x * 1.1f;
+            var drawSize = 9 / NoteCanvas.ScaleFactor.Value;
             foreach (var noteObj in EditData.Notes.Values)
             {
+                // 通常ノードの描画
                 var canvasPosOfNote = ConvertUtils.NoteToCanvasPosition(noteObj.note.position);
-                var min = ConvertUtils.ScreenToCanvasPosition(Vector3.zero).x;
-                var max = ConvertUtils.ScreenToCanvasPosition(Vector3.right * Screen.width).x * 1.1f;
-
                 if (min <= canvasPosOfNote.x && canvasPosOfNote.x <= max)
                 {
                     noteObj.LateUpdateObservable.OnNext(Unit.Default);
                     var screenPos = ConvertUtils.CanvasToScreenPosition(canvasPosOfNote);
-                    var drawSize = 9 / NoteCanvas.ScaleFactor.Value;
                     GLQuadDrawer.Draw(new Geometry(
                         new[] {
                             new Vector3(screenPos.x, screenPos.y - drawSize, 0),
@@ -30,12 +32,31 @@ namespace NoteEditor.GLDrawing
                             new Vector3(screenPos.x, screenPos.y + drawSize, 0),
                             new Vector3(screenPos.x - drawSize, screenPos.y, 0)
                         },
-                        noteObj.NoteColor));
+                        noteObj.NoteColor)
+                    );
 
                     if (noteObj.note.type == Notes.NoteTypes.Long && EditData.Notes.ContainsKey(noteObj.note.prev))
                     {
                         EditData.Notes[noteObj.note.prev].LateUpdateObservable.OnNext(Unit.Default);
                     }
+                }
+
+                // 手本の描画
+                var modelPos = noteObj.note.position;
+                modelPos.num += modelPos.LPB * EditData.ModelSize.Value * 4;
+                canvasPosOfNote = ConvertUtils.NoteToCanvasPosition(modelPos);
+                if (min <= canvasPosOfNote.x && canvasPosOfNote.x <= max && 0 < EditData.ModelSize.Value)
+                {
+                    var screenPos = ConvertUtils.CanvasToScreenPosition(canvasPosOfNote);
+                    GLQuadDrawer.Draw(new Geometry(
+                        new[] {
+                            new Vector3(screenPos.x, screenPos.y - drawSize, 0),
+                            new Vector3(screenPos.x + drawSize, screenPos.y, 0),
+                            new Vector3(screenPos.x, screenPos.y + drawSize, 0),
+                            new Vector3(screenPos.x - drawSize, screenPos.y, 0)
+                        },
+                        modelColor)
+                    );
                 }
             }
         }
